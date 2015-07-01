015f9eac57e02947ea229e2c5d496b07f8495f4d
Sam Roeca <samuel.roeca@gmail.com>
2015-06-30 23:40:57 -0400

    Added config step

---
+++ config.py
@@ -0,0 +1,4 @@
+from errorcatch import error_handle_test
+
+# This module checks whether this can be successfully configured pre-use
+error_handler = error_handle_test("This is the message")
--- testmain.py
+++ testmain.py
@@ -1,11 +1,12 @@
 import testmod
-from errorcatch import error_handle_test
+from config import error_handler
 
 def add_one(n):
     pass
     return testmod.div_by_zero(n) + 1
 
-@error_handle_test("Error during fun process")
+
+@error_handler
 def main_module():
     print "Hello"
     print "World"
